59df80f7556356da82f44ff6dbeb54d7884043e5
maximrakov <78982680+maximrakov@users.noreply.github.com>
2022-11-04 16:44:47 +0300

    git commit -m "CA-1 bootstrap project"

---
+++ src/test/java/devtools/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package devtools.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
